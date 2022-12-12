dcb631feb5270f49da92adcafcca99b44411e3a7
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2022-12-12 11:24:52 +0530

    Update REVERSE_LINKLIST.cpp

--- REVERSE_LINKLIST.cpp
+++ REVERSE_LINKLIST.cpp
@@ -1,1 +1,39 @@
-vfdgfdgdfdf
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+//////////////////////////////ITTRATIVE APPRTOCH 1////////////////////////////////////////////
+Node* reverseLinkedList(Node *head)
+{
+   if(head== NULL || head->==NULL){
+     return head;
+   }
+    Node* prev=NULL;
+    Node* curr=head;
+    Node* forward=NULL;
+    while(curr!=NULL){
+        forward  = curr ->next;
+        curr ->next=prev=;
+        prev=curr;
+        curr=forward;
+        
+        
+        
+    }
+    return prev;
+    
+}
