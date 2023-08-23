efbb2565a86cd7ec82db820c09a9d48bfff10ea3
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-08-24 00:55:22 +0530

    Update Find_Middle_ele.cpp

--- Revision/Find_Middle_ele.cpp
+++ Revision/Find_Middle_ele.cpp
@@ -24,3 +24,26 @@ class Solution{
         return ptr->data;
     }
 };
+/**************************************************************************************************/
+2 pointer way
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        
+        struct Node *slow,*fast;
+      
+        slow=head;
+        fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        if(slow==NULL)
+        return -1;
+        return slow->data;
+    }
+};
